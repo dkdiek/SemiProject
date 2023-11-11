c9aa88d8d5c249d4535ba3236bd0edf20c089025
k <developerlyk@gmail.com>
2023-11-11 21:41:16 +0900

    1111

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ io.on("connection",(socket)=>{
         io.emit("chatting", {
             name: name,
             msg : msg,
-            time : moment(new Date()).format("h:ss A")
+            time : moment(new Date()).format("h:mm A")
         })
     })
 })
